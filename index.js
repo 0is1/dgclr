a14ad001320add6067585f01396f57e04474f210
Janne Saarela <janne.saarela@kehittamo.fi>
2017-10-25 22:15:46 +0300

    Add new looping logic

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import cheerio from 'cheerio';
 import {take} from 'lodash';
 import data from './data.json';
 
+require('./db');
 // request('https://frisbeegolfradat.fi/radat/', (error, response, body) => {
 //     console.log('statusCode:', response && response.statusCode);
 // Print the response status code if a response was received
@@ -20,7 +21,7 @@ import data from './data.json';
 //     //     console.log('The file has been saved!');
 //     // });
 // });
-const someData = take(data, 5);
+const someData = take(data, 3);
 someData.forEach(course =>
     request(course.url, (error, response, body) => {
         const $ = cheerio.load(body);
@@ -32,6 +33,13 @@ someData.forEach(course =>
                 console.log('Väylien määrä: ', $(info).children('p').text());
             } else if ($(info).children('b').text().toLowerCase() === 'pinnanmuodot') {
                 console.log('Pinnanmuodot: ', $(info).children('p').text());
+            }
+        });
+        // $('.fairway h4').each((i, item) => console.log('Väylä: ', $(item).text()));
+        $('.fairway p').each((i, item) => {
+            if (!(i % 2)) {
+                const lengthAndPar = $(item).text().replace(/[A-Öa-ö ]/g, '').split('.');
+                console.log(lengthAndPar);
             }
         });
         // console.log($('span.caption').text());
