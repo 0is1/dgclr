1d2d06503155a39c7c1c25460b6141e63f3d1c8c
Janne Saarela <janne.juhani@gmail.com>
2018-08-27 12:29:49 +0300

    Add Input component

---
+++ components/Input/Input.styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Styles = {
+  Input: styled.input`
+    line-height: 1.25;
+    padding: 0.5rem;
+    width: 100%;
+  `,
+};
+
+export default Styles;
---
+++ components/Input/index.js
@@ -0,0 +1,13 @@
+// @flow
+import React from 'react';
+import Styles from './Input.styles';
+
+// type Props = {
+//   children: Node,
+// };
+
+const { Input } = Styles;
+
+const InputComponent = () => <Input />;
+
+export default InputComponent;
