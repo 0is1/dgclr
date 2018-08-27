a156222f0ecd212e99028d43cbc9e91a611d566f
Janne Saarela <janne.juhani@gmail.com>
2018-08-27 11:11:54 +0300

    Add flowtype deps and config

--- .babelrc.js
+++ .babelrc.js
@@ -1,69 +1,73 @@
 const env = require('./env-config');
 module.exports = {
-    env: {
-        development: {
-            presets: ['next/babel'],
-            plugins: [
-                [
-                    'styled-components',
-                    {
-                        ssr: true,
-                        displayName: true,
-                        preprocess: false,
-                    },
-                ],
-            ],
-        },
-        production: {
-            presets: ['next/babel'],
-            plugins: [
-                [
-                    'styled-components',
-                    {
-                        ssr: true,
-                        displayName: true,
-                        preprocess: false,
-                    },
-                ],
-            ],
-        },
-        test: {
-            presets: [
-                [
-                    'env',
-                    {
-                        modules: 'commonjs',
-                    },
-                ],
-                'next/babel',
-            ],
-            plugins: [
-                [
-                    'styled-components',
-                    {
-                        ssr: true,
-                        displayName: true,
-                        preprocess: false,
-                    },
-                ],
-            ],
-        },
+  env: {
+    development: {
+      presets: ['next/babel'],
+      plugins: [
+        'transform-flow-strip-types',
+        [
+          'styled-components',
+          {
+            ssr: true,
+            displayName: true,
+            preprocess: false,
+          },
+        ],
+      ],
     },
-    plugins: [
+    production: {
+      presets: ['next/babel'],
+      plugins: [
+        'transform-flow-strip-types',
         [
-            'styled-components',
-            {
-                ssr: true,
-                displayName: true,
-                preprocess: false,
-            },
+          'styled-components',
+          {
+            ssr: true,
+            displayName: true,
+            preprocess: false,
+          },
         ],
+      ],
+    },
+    test: {
+      presets: [
         [
-            'module-resolver',
-            {
-                root: ['./'],
-            },
+          'env',
+          {
+            modules: 'commonjs',
+          },
         ],
-        ['transform-define', env],
+        'next/babel',
+      ],
+      plugins: [
+        'transform-flow-strip-types',
+        [
+          'styled-components',
+          {
+            ssr: true,
+            displayName: true,
+            preprocess: false,
+          },
+        ],
+      ],
+    },
+  },
+  plugins: [
+    'transform-flow-strip-types',
+    [
+      'styled-components',
+      {
+        ssr: true,
+        displayName: true,
+        preprocess: false,
+      },
     ],
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+      },
+    ],
+    ['transform-define', env],
+  ],
 };
