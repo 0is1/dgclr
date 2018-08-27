853f3180caad2da150ad9de7d9fac767b1c5749d
Janne Saarela <janne.juhani@gmail.com>
2018-08-27 12:00:10 +0300

    Add initial Container component

---
+++ components/Container/Container.styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Styles = {
+  Container: styled.div`
+    background-color: rgb(83, 228, 75);
+    color: white;
+    padding: 2rem;
+  `,
+};
+
+export default Styles;
---
+++ components/Container/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Styles from './Container.styles';
+
+type Props = {
+  children: Node,
+};
+
+const { Container } = Styles;
+
+export default ({ children }: Props) => <Container>{children}</Container>;
