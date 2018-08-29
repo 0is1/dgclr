bb29de23ecc27d2ab5acf5c3a196d238ee6a1cae
Janne Saarela <janne.juhani@gmail.com>
2018-08-29 21:35:21 +0300

    Add new styles and more content

--- components/Course/Course.js
+++ components/Course/Course.js
@@ -1,49 +1,219 @@
 // @flow
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+import { withRouter } from 'next/router';
 import { connect } from 'react-redux';
 import { size } from 'lodash';
 import ReactHtmlParser from 'react-html-parser';
+import {
+  BackgroundImage, Card, Flex, Subhead, Text,
+} from 'rebass';
+import {
+  convertLinksToHtml,
+  convertCoordinatesToObject,
+  courseAddressDetails,
+  uniqueLayoutRatings,
+} from 'helpers/utils';
 import { courseBySlugFromState } from 'components/Course/selectors';
+import { setCourses as setCoursesFunc } from 'components/SearchContainer/actions';
+import Map from 'components/Map';
+import Layouts from 'components/Layouts';
+import Tabs from 'components/Tabs';
+import LayoutRatingBadges from 'components/Layout/Badges';
 import Styles from 'components/Course/Course.styles';
-import ContainerStyles from 'components/Container/Container.styles';
 
 type Props = {
   course: {},
-  slug: string,
+  data?: {},
+  setCourses: Function,
+  // slug: string,
 };
 
-const { Description, Title, Wrapper } = Styles;
-const { Container } = ContainerStyles;
-const convertLinks = string => string.replace(/((https?|ftp):\/\/[^\s/$.?#].[^\s]*)/gi, "<a href='$1'>$1</a>");
+const {
+  Box, Description, PanelWrapper, PanelHeader, PanelFooter, Strong, Title, Wrapper,
+} = Styles;
 
 class Course extends Component<Props> {
   componentDidMount() {
-    const { course, slug } = this.props;
-    if (size(course) < 1) {
-      console.log('Load course: ', slug);
+    const { course, data } = this.props;
+    const { courseBySlug } = data;
+    if (size(course) < 1 && courseBySlug && courseBySlug.length) {
+      this.setCourses(courseBySlug);
     }
   }
 
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (snapshot !== null) {
+      const { course, data } = this.props;
+      const { courseBySlug } = data;
+      if (size(course) < 1 && courseBySlug && courseBySlug.length) {
+        this.setCourses(courseBySlug);
+      }
+    }
+  }
+
+  getSnapshotBeforeUpdate(prevProps) {
+    const { data } = this.props;
+    const { courseBySlug } = data;
+    if (size(prevProps.course) < 1 && courseBySlug && courseBySlug.length) {
+      return true;
+    }
+    return null;
+  }
+
+  setCourses = (courses) => {
+    const { setCourses } = this.props;
+    setCourses(courses);
+  };
+
   render() {
-    const { course } = this.props;
-    if (size(course) < 1) {
+    const { course, data } = this.props;
+    if (size(course) < 1 || (data && data.loading)) {
       return <h1>Loading...</h1>;
     }
-    console.log(course);
-    const { name, description } = course;
-    const descriptionWithLinks = convertLinks(description);
+    console.log('course: ', course);
+    const {
+      name, courseInfo, description, locationInfo, layouts,
+    } = course;
+    const descriptionWithLinks = convertLinksToHtml(description);
+    const coordinates = convertCoordinatesToObject(
+      locationInfo.location && locationInfo.location.coordinates,
+    );
+    const layoutNames = layouts.map(layout => layout.name);
+    // eslint-disable-next-line no-underscore-dangle
+    const layoutTabs = <Tabs tabs={layoutNames} id={course._id} />;
+    const mapElement = coordinates ? (
+      <Map coordinates={coordinates} />
+    ) : (
+      <BackgroundImage
+        ratio={1}
+        src="https://images.unsplash.com/photo-1462331940025-496dfbfc7564?w=2048&q=20"
+      />
+    );
+    const ratings = uniqueLayoutRatings(layouts);
     return (
-      <Container>
-        <Wrapper>
-          <Title>{name}</Title>
-          <Description>{ReactHtmlParser(descriptionWithLinks)}</Description>
-        </Wrapper>
-      </Container>
+      <Wrapper>
+        <PanelWrapper>
+          <PanelHeader>Radan tiedot</PanelHeader>
+          <Flex>
+            <Box width="50%" p={4}>
+              <Title>
+                {name}
+                <LayoutRatingBadges ratings={ratings} />
+              </Title>
+              <Description>{ReactHtmlParser(descriptionWithLinks)}</Description>
+              <PanelWrapper mt={4}>
+                <PanelHeader>Radan lisätiedot:</PanelHeader>
+                <Box width="100%" px={1} py={2}>
+                  <Text mt=".85rem" pl={3}>
+                    <Strong>Korityyppi: </Strong>
+                    {`${courseInfo.basketType}`}
+                  </Text>
+                  <Text mt=".85rem" pl={3}>
+                    <Strong>Opastaulut: </Strong>
+                    {`${courseInfo.infoSignType}`}
+                  </Text>
+                  <Text mt=".85rem" pl={3}>
+                    <Strong>Heittoalustat: </Strong>
+                    {`${courseInfo.teeType}`}
+                  </Text>
+                  <Text mt=".85rem" pl={3}>
+                    <Strong>Ratatyypit: </Strong>
+                    {`${courseInfo.courseTypes.join(', ')}`}
+                  </Text>
+                  <Text mt=".85rem" mb=".85rem" pl={3}>
+                    <Strong>Pinnanmuodot: </Strong>
+                    {`${courseInfo.surfaceShapeTypes.join('. ')}`}
+                  </Text>
+                </Box>
+              </PanelWrapper>
+            </Box>
+            <Box width="50%" p={4}>
+              <Card width="100%">
+                {mapElement}
+                <Subhead mt={2} p={2}>
+                  {courseAddressDetails(locationInfo)}
+                </Subhead>
+              </Card>
+            </Box>
+          </Flex>
+          <PanelFooter>
+            {layoutTabs}
+            {/* eslint-disable-next-line no-underscore-dangle */}
+            <Layouts id={course._id} layouts={layouts} />
+          </PanelFooter>
+        </PanelWrapper>
+      </Wrapper>
     );
   }
 }
+
+Course.defaultProps = {
+  data: {},
+};
 const mapStateToProps = (state, ownProps) => ({
   course: courseBySlugFromState(state, ownProps),
 });
 
-export default connect(mapStateToProps)(Course);
+const mapDispatchToProps = dispatch => ({
+  setCourses: courses => dispatch(setCoursesFunc(courses)),
+});
+
+const SEARCH_COURSE = gql`
+  query CourseBySlugQuery($slug: String!) {
+    courseBySlug(slug: $slug) {
+      _id
+      name
+      description
+      slug
+      courseInfo {
+        basketType
+        teeType
+        infoSignType
+        surfaceShapeTypes
+        courseTypes
+      }
+      locationInfo {
+        address
+        city
+        zip
+        location {
+          coordinates
+        }
+      }
+      layouts {
+        name
+        rating
+        holes {
+          par
+          length {
+            foot
+            meter
+          }
+        }
+        holeCount
+        totalPar
+      }
+    }
+  }
+`;
+
+// The `graphql` wrapper executes a GraphQL query and makes the results
+// available on the `data` prop of the wrapped component (PostList)
+const ComponentWithMutation = graphql(SEARCH_COURSE, {
+  options: ({ router: { query } }) => ({
+    variables: {
+      slug: query.slug,
+    },
+  }),
+  props: ({ data }) => ({
+    data,
+  }),
+  skip: ({ course }) => size(course) > 0,
+})(Course);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(withRouter(ComponentWithMutation));
--- components/Course/Course.styles.js
+++ components/Course/Course.styles.js
@@ -1,17 +1,32 @@
 import styled from 'styled-components';
+import {
+  Box, Heading, Panel, Text,
+} from 'rebass';
+import colors from 'components/colors';
 
 const Styles = {
   Wrapper: styled.div`
     margin: 1rem;
-    padding: 2rem;
+    padding: 1rem;
   `,
-  Title: styled.h2`
-    font-size: 3rem;
-    font-weight: 800;
+  Title: styled(Heading)`
+    color: ${colors.text};
+    margin-bottom: 1.5rem;
   `,
-  Description: styled.p`
+  Description: styled(Text)`
+    color: ${colors.text};
     font-weight: 200;
-    line-height: 1.25;
+    line-height: 1.4;
+  `,
+  Box: styled(Box)``,
+  PanelWrapper: styled(Panel)``,
+  PanelHeader: styled(Panel.Header)`
+    background: ${colors.info};
+    color: ${colors.white};
+  `,
+  PanelFooter: styled(Panel.Footer)``,
+  Strong: styled.span`
+    font-weight: 700;
   `,
 };
 
