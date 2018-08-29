0b22a3370a0384a8242807687cf1dc047a2d031b
Janne Saarela <janne.juhani@gmail.com>
2018-08-29 21:36:13 +0300

    Add header and footer to Container

--- components/Container/Container.styles.js
+++ components/Container/Container.styles.js
@@ -8,6 +8,13 @@ const Styles = {
     flex-direction: column;
     width: 100%;
   `,
+  HeaderLink: styled.span`
+    display: block;
+    font-size: 1rem;
+    color: #262626;
+    line-height: 1.5;
+    cursor: pointer;
+  `,
 };
 
 export default Styles;
--- components/Container/index.js
+++ components/Container/index.js
@@ -1,11 +1,42 @@
 // @flow
 import React from 'react';
+import Link from 'next/link';
+import { Tabs, Tab, Text } from 'rebass';
 import Styles from './Container.styles';
 
 type Props = {
+  activeRoute?: string,
   children: Node,
 };
 
-const { Container } = Styles;
+const { Container, HeaderLink } = Styles;
 
-export default ({ children }: Props) => <Container>{children}</Container>;
+const ContainerComponent = ({ activeRoute, children }: Props) => (
+  <React.Fragment>
+    <header>
+      <Tabs px={4} pt={2}>
+        <Tab borderColor={activeRoute === '/' ? 'blue' : 'transparent'} px={3}>
+          <Link href="/">
+            <HeaderLink>Haku</HeaderLink>
+          </Link>
+        </Tab>
+        {/* <Tab px={3}>
+          <Link href="/">
+            <HeaderLink>Info</HeaderLink>
+          </Link>
+        </Tab> */}
+      </Tabs>
+    </header>
+    <Container>{children}</Container>
+    <footer>
+      <Text mt={2} textAlign="center">
+        DGCLR ei vastaa sivujen sisällön oikeellisuudesta tai virheettömyydestä!
+      </Text>
+    </footer>
+  </React.Fragment>
+);
+ContainerComponent.defaultProps = {
+  activeRoute: '',
+};
+
+export default ContainerComponent;
