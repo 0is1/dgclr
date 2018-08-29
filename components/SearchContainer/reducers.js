c295526a106ac191a16631e9bfd445f39aab6da9
Janne Saarela <janne.juhani@gmail.com>
2018-08-29 21:33:47 +0300

    Add SET_SEARCH_QUERY reducer

--- components/SearchContainer/reducers.js
+++ components/SearchContainer/reducers.js
@@ -5,13 +5,24 @@ import { actionTypes } from './actions';
 
 export const initialState = {
   courses: {},
+  queries: {},
+  queryHistory: [],
   error: false,
 };
+
+const addQueryHistory = (queryHistory, query) => [].concat([query], queryHistory);
 
 const reducer = createReducer({
   [actionTypes.SET_COURSES]: (state, action) => {
     const courses = keyBy(course => course.slug, action.courses);
     return update({ courses }, state);
+  },
+  [actionTypes.SET_SEARCH_QUERY]: (state, action) => {
+    const { courses, query } = action;
+    return update(
+      { queries: { [query]: courses }, queryHistory: addQueryHistory(state.queryHistory, query) },
+      state,
+    );
   },
 });
 
