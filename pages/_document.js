a01ff48d151b2f192dbb11a354bd3f580214fbe5
Janne Saarela <janne.juhani@gmail.com>
2018-08-30 13:42:07 +0300

    Add meta viewport

--- pages/_document.js
+++ pages/_document.js
@@ -54,6 +54,7 @@ export default class extends Document {
         <Head>
           {this.helmetJsx}
           {this.helmetHeadComponents}
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css"
