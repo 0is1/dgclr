255c602418840b7deb669f5aca5986882d989cfa
Janne Saarela <janne.juhani@gmail.com>
2018-09-30 11:38:06 +0300

    Add actions tests for SearchContainer

---
+++ components/SearchContainer/mock/courses.mock.js
@@ -0,0 +1,480 @@
+export default [
+  {
+    _id: '5b86d88c88ad697a0c00b948',
+    courseInfo: {
+      fee: {
+        value: 'ilmainen',
+      },
+      courseTypes: ['Metsärata'],
+      surfaceShapeTypes: ['Mäkinen, paljon korkeuseroja'],
+      founded: '2015',
+      basketType: 'Prodiscus',
+      teeType: 'Tekonurmi',
+      infoSignType: 'Muu',
+      maintenanceCycle: 'Viikoittainen',
+      rangeMaster: 'Sami Väisänen ja Kari Liimatainen',
+      courseDesigner: 'Sami Väisänen, Kari Liimatainen ja Juha Sormunen',
+      mapUrl: 'https://frisbeegolfradat.fi/files/2018/06/44200_suolahti_ratakartta_2018.jpg',
+    },
+    locationInfo: {
+      address: 'Telakkakatu 15',
+      city: 'Äänekoski',
+      zip: '44200',
+      location: {
+        type: 'Point',
+        coordinates: [62.5633, 25.8686],
+      },
+    },
+    events: [],
+    layouts: [
+      {
+        _id: '5b86d88c88ad697a0c00b949',
+        name: '44200 Suolahti',
+        holes: [
+          {
+            length: {
+              foot: 196,
+              meter: 60,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 209,
+              meter: 64,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 219,
+              meter: 67,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 301,
+              meter: 92,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 177,
+              meter: 54,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 232,
+              meter: 71,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 141,
+              meter: 43,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 314,
+              meter: 96,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 223,
+              meter: 68,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 173,
+              meter: 53,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 285,
+              meter: 87,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 242,
+              meter: 74,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 193,
+              meter: 59,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 229,
+              meter: 70,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 203,
+              meter: 62,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 439,
+              meter: 134,
+            },
+            par: 4,
+          },
+          {
+            length: {
+              foot: 226,
+              meter: 69,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 226,
+              meter: 69,
+            },
+            par: 3,
+          },
+        ],
+        rating: 'A1',
+        holeCount: 18,
+        totalPar: 55,
+      },
+    ],
+    name: '44200 Suolahti',
+    description: '',
+    slug: '44200-suolahti-äänekoski',
+    created: '2018-08-29T17:31:56.831Z',
+    updatedAt: '2018-08-29T17:31:56.832Z',
+    __v: 0,
+  },
+  {
+    _id: '5b86d8ea88ad697a0c00b99c',
+    courseInfo: {
+      fee: {
+        value: 'ilmainen',
+      },
+      courseTypes: ['Peltorata', 'Metsärata'],
+      surfaceShapeTypes: ['Kumpuileva, jonkin verran korkeuseroja'],
+      founded: '2017',
+      basketType: 'DiscGolfPark-maalikori',
+      teeType: 'TeePad',
+      infoSignType: 'TeeSign',
+      maintenanceCycle: 'Kuukausittainen',
+      rangeMaster: 'Frisbeegolfclub Hyrylä ry',
+      courseDesigner: 'Jussi Meresmaa',
+      mapUrl: '#',
+    },
+    locationInfo: {
+      address: 'Kilpailukuja',
+      city: 'Tuusula',
+      zip: '04300',
+      location: {
+        type: 'Point',
+        coordinates: [60.3905, 25.03],
+      },
+    },
+    events: [],
+    layouts: [
+      {
+        _id: '5b86d8ea88ad697a0c00b99e',
+        name: 'Ford DiscGolfPark, Punainen',
+        holes: [
+          {
+            length: {
+              foot: 432,
+              meter: 132,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 183,
+              meter: 56,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 590,
+              meter: 180,
+            },
+            par: 4,
+          },
+          {
+            length: {
+              foot: 242,
+              meter: 74,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 531,
+              meter: 162,
+            },
+            par: 4,
+          },
+          {
+            length: {
+              foot: 275,
+              meter: 84,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 308,
+              meter: 94,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 295,
+              meter: 90,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 252,
+              meter: 77,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 393,
+              meter: 120,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 698,
+              meter: 213,
+            },
+            par: 4,
+          },
+          {
+            length: {
+              foot: 311,
+              meter: 95,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 308,
+              meter: 94,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 403,
+              meter: 123,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 334,
+              meter: 102,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 328,
+              meter: 100,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 406,
+              meter: 124,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 324,
+              meter: 99,
+            },
+            par: 3,
+          },
+        ],
+        rating: 'AA1',
+        holeCount: 18,
+        totalPar: 57,
+      },
+      {
+        _id: '5b86d8ea88ad697a0c00b99d',
+        name: 'Ford DiscGolfPark, Sininen',
+        holes: [
+          {
+            length: {
+              foot: 724,
+              meter: 221,
+            },
+            par: 4,
+          },
+          {
+            length: {
+              foot: 229,
+              meter: 70,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 590,
+              meter: 180,
+            },
+            par: 4,
+          },
+          {
+            length: {
+              foot: 242,
+              meter: 74,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 531,
+              meter: 162,
+            },
+            par: 4,
+          },
+          {
+            length: {
+              foot: 275,
+              meter: 84,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 308,
+              meter: 94,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 295,
+              meter: 90,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 255,
+              meter: 78,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 393,
+              meter: 120,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 698,
+              meter: 213,
+            },
+            par: 4,
+          },
+          {
+            length: {
+              foot: 459,
+              meter: 140,
+            },
+            par: 4,
+          },
+          {
+            length: {
+              foot: 308,
+              meter: 94,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 442,
+              meter: 135,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 334,
+              meter: 102,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 328,
+              meter: 100,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 419,
+              meter: 128,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 324,
+              meter: 99,
+            },
+            par: 3,
+          },
+        ],
+        rating: 'AA1',
+        holeCount: 18,
+        totalPar: 59,
+      },
+    ],
+    name: 'Ford DiscGolfPark',
+    description:
+      'Ford DiscGolfPark on kilpatason frisbeegolfrata Tuusulan Rykmentinpuistossa. Rata on rakennettu yhteisteistyössä Tuusulan Kunnan, FrisbeeGolfClub Hyrylä ry:n ja Oy Ford Ab:n kanssa.\nPaikallinen seura on jo ryhtynyt järjestämään viikkokisoja radalla. Voit seurata tiedotusta Facebookin kautta:\nFrisbeegolfclub Hyrylä ry\nFord DiscGolfPark',
+    slug: 'ford-discgolfpark-tuusula',
+    created: '2018-08-29T17:33:30.754Z',
+    updatedAt: '2018-08-29T17:33:30.754Z',
+    __v: 0,
+  },
+];
