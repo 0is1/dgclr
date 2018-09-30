8726d8b515fd8cc1c34b0579a9a236a803d96e9f
Janne Saarela <janne.juhani@gmail.com>
2018-09-30 12:26:07 +0300

    Add Course selectors tests

---
+++ components/Course/selectors.test.js
@@ -0,0 +1,27 @@
+/* eslint-env jest */
+import mockCoursesData from 'components/SearchContainer/mock/courses.mock';
+import { DEFAULT_STATE } from 'lib/rootReducer';
+import { courseBySlugFromState } from './selectors';
+
+describe('SearchContainer selectors', () => {
+  it('Select courseBySlugFromState with initialState', () => {
+    const result = courseBySlugFromState(DEFAULT_STATE, 'query');
+    expect(result).toEqual({});
+  });
+  it('Select courseBySlugFromState with return value', () => {
+    const ownProps = { slug: mockCoursesData[1].slug };
+    const result = courseBySlugFromState(
+      {
+        ...DEFAULT_STATE,
+        search: {
+          courses: {
+            [mockCoursesData[0].slug]: mockCoursesData[0],
+            [mockCoursesData[1].slug]: mockCoursesData[1],
+          },
+        },
+      },
+      ownProps,
+    );
+    expect(result).toEqual(mockCoursesData[1]);
+  });
+});
