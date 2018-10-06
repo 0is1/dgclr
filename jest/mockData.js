83ea761028b1c6099f608ef454a500de8f28da87
Janne Saarela <janne.juhani@gmail.com>
2018-10-06 13:12:47 +0300

    Add mock data

---
+++ jest/mockData.js
@@ -0,0 +1,640 @@
+export const mockCourses = [
+  {
+    _id: '5b86dc3e88ad697a0c00bcdd',
+    courseInfo: {
+      fee: {
+        value: 'ilmainen',
+      },
+      courseTypes: ['Metsärata'],
+      surfaceShapeTypes: ['Kumpuileva, jonkin verran korkeuseroja'],
+      founded: '2015',
+      basketType: 'DiscGolfPark-maalikori',
+      teeType: 'Tekonurmi',
+      infoSignType: 'Itsetehdyt opasteet',
+      maintenanceCycle: 'Kuukausittainen',
+      rangeMaster: 'Antti G., Kaj S. ja Sami A.',
+      courseDesigner: 'Puskasoturit ry',
+      mapUrl: 'https://frisbeegolfradat.fi/files/2017/10/puolarmaarin_espoo_ratakartta_2017.jpg',
+    },
+    locationInfo: {
+      address: 'Puolarmaari 12',
+      city: 'Espoo',
+      zip: '02210',
+      location: {
+        type: 'Point',
+        coordinates: [60.1766, 24.6981],
+      },
+    },
+    events: [],
+    layouts: [
+      {
+        _id: '5b86dc3e88ad697a0c00bcde',
+        name: 'Puolarmaarin frisbeegolfrata',
+        holes: [
+          {
+            length: {
+              foot: 259,
+              meter: 79,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 239,
+              meter: 73,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 216,
+              meter: 66,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 186,
+              meter: 57,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 216,
+              meter: 66,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 213,
+              meter: 65,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 196,
+              meter: 60,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 150,
+              meter: 46,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 164,
+              meter: 50,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 223,
+              meter: 68,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 170,
+              meter: 52,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 157,
+              meter: 48,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 196,
+              meter: 60,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 232,
+              meter: 71,
+            },
+            par: 3,
+          },
+        ],
+        rating: 'A1',
+        holeCount: 14,
+        totalPar: 42,
+      },
+    ],
+    name: 'Puolarmaarin frisbeegolfrata',
+    description:
+      'Puolarmaarin 14-väyläisen radan ratakartta\nRata on saavuttanut keväällä 2016 muotonsa 14-väyläisenä perheratana. Ratakartta löytyy väylän nro. 1 välittömästä läheisyydestä ja väyläkartat jokaisen väylän tiin vieressä.\nOpastaulu löytyy osoitteesta: https://drive.google.com/open?id=0B0nxeLmzG4z8OEhOTEJJTnVoWjg',
+    slug: 'puolarmaarin-frisbeegolfrata-espoo',
+    created: '2018-08-29T17:47:42.365Z',
+    updatedAt: '2018-08-29T17:47:42.365Z',
+    __v: 0,
+  },
+  {
+    _id: '5b86da5888ad697a0c00bb0f',
+    courseInfo: {
+      fee: {
+        value: 'ilmainen',
+      },
+      courseTypes: ['Metsärata'],
+      surfaceShapeTypes: ['Mäkinen, paljon korkeuseroja'],
+      founded: '2016',
+      basketType: 'Prodigy',
+      teeType: 'Tekonurmi',
+      infoSignType: 'Itsetehdyt opasteet',
+      maintenanceCycle: 'Viikoittainen',
+      rangeMaster: 'Tapio Virnes',
+      courseDesigner: 'Tapio Virnes',
+      mapUrl: '#',
+    },
+    locationInfo: {
+      address: 'Golftie 10-12',
+      city: 'Joroinen',
+      zip: '79600',
+      location: {
+        type: 'Point',
+        coordinates: [62.179, 27.8813],
+      },
+    },
+    events: [],
+    layouts: [
+      {
+        _id: '5b86da5888ad697a0c00bb13',
+        name: 'Kanavan frisbeegolf',
+        holes: [
+          {
+            length: {
+              foot: 334,
+              meter: 102,
+            },
+            par: 4,
+          },
+          {
+            length: {
+              foot: 232,
+              meter: 71,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 301,
+              meter: 92,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 350,
+              meter: 107,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 147,
+              meter: 45,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 669,
+              meter: 204,
+            },
+            par: 5,
+          },
+          {
+            length: {
+              foot: 360,
+              meter: 110,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 203,
+              meter: 62,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 364,
+              meter: 111,
+            },
+            par: 4,
+          },
+          {
+            length: {
+              foot: 196,
+              meter: 60,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 633,
+              meter: 193,
+            },
+            par: 5,
+          },
+          {
+            length: {
+              foot: 328,
+              meter: 100,
+            },
+            par: 4,
+          },
+          {
+            length: {
+              foot: 160,
+              meter: 49,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 282,
+              meter: 86,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 157,
+              meter: 48,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 249,
+              meter: 76,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 505,
+              meter: 154,
+            },
+            par: 4,
+          },
+          {
+            length: {
+              foot: 288,
+              meter: 88,
+            },
+            par: 3,
+          },
+        ],
+        rating: 'AA1',
+        holeCount: 18,
+        totalPar: 62,
+      },
+      {
+        _id: '5b86da5888ad697a0c00bb12',
+        name: 'Kotkatharjun frisbeegolf',
+        holes: [
+          {
+            length: {
+              foot: 396,
+              meter: 121,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 455,
+              meter: 139,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 436,
+              meter: 133,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 514,
+              meter: 157,
+            },
+            par: 4,
+          },
+          {
+            length: {
+              foot: 459,
+              meter: 140,
+            },
+            par: 4,
+          },
+          {
+            length: {
+              foot: 557,
+              meter: 170,
+            },
+            par: 4,
+          },
+          {
+            length: {
+              foot: 390,
+              meter: 119,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 308,
+              meter: 94,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 511,
+              meter: 156,
+            },
+            par: 4,
+          },
+          {
+            length: {
+              foot: 380,
+              meter: 116,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 570,
+              meter: 174,
+            },
+            par: 4,
+          },
+          {
+            length: {
+              foot: 524,
+              meter: 160,
+            },
+            par: 4,
+          },
+          {
+            length: {
+              foot: 223,
+              meter: 68,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 429,
+              meter: 131,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 721,
+              meter: 220,
+            },
+            par: 5,
+          },
+          {
+            length: {
+              foot: 770,
+              meter: 235,
+            },
+            par: 5,
+          },
+          {
+            length: {
+              foot: 436,
+              meter: 133,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 314,
+              meter: 96,
+            },
+            par: 3,
+          },
+        ],
+        rating: 'AAA1',
+        holeCount: 18,
+        totalPar: 64,
+      },
+      {
+        _id: '5b86da5888ad697a0c00bb11',
+        name: 'Kotkatharjun lämmittelyväylät',
+        holes: [
+          {
+            length: {
+              foot: 239,
+              meter: 73,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 360,
+              meter: 110,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 308,
+              meter: 94,
+            },
+            par: 3,
+          },
+        ],
+        rating: 'B1',
+        holeCount: 3,
+        totalPar: 9,
+      },
+      {
+        _id: '5b86da5888ad697a0c00bb10',
+        name: 'Kotkatharjun ulkoilualue',
+        holes: [
+          {
+            length: {
+              foot: 239,
+              meter: 73,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 360,
+              meter: 110,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 308,
+              meter: 94,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 334,
+              meter: 102,
+            },
+            par: 4,
+          },
+          {
+            length: {
+              foot: 232,
+              meter: 71,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 301,
+              meter: 92,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 350,
+              meter: 107,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 147,
+              meter: 45,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 669,
+              meter: 204,
+            },
+            par: 5,
+          },
+          {
+            length: {
+              foot: 360,
+              meter: 110,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 203,
+              meter: 62,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 364,
+              meter: 111,
+            },
+            par: 4,
+          },
+          {
+            length: {
+              foot: 380,
+              meter: 116,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 570,
+              meter: 174,
+            },
+            par: 4,
+          },
+          {
+            length: {
+              foot: 524,
+              meter: 160,
+            },
+            par: 4,
+          },
+          {
+            length: {
+              foot: 223,
+              meter: 68,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 429,
+              meter: 131,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 721,
+              meter: 220,
+            },
+            par: 5,
+          },
+          {
+            length: {
+              foot: 770,
+              meter: 235,
+            },
+            par: 5,
+          },
+          {
+            length: {
+              foot: 436,
+              meter: 133,
+            },
+            par: 3,
+          },
+          {
+            length: {
+              foot: 314,
+              meter: 96,
+            },
+            par: 3,
+          },
+        ],
+        rating: 'AAA1',
+        holeCount: 21,
+        totalPar: 73,
+      },
+    ],
+    name: 'Kotkatharjun ulkoilualue',
+    description: '',
+    slug: 'kotkatharjun-ulkoilualue-joroinen',
+    created: '2018-08-29T17:39:36.229Z',
+    updatedAt: '2018-08-29T17:39:36.229Z',
+    __v: 0,
+  },
+];
+
+export default mockCourses;
