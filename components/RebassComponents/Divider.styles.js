d0abd12dc2c8e78c9477dcb137717965520ff3dc
Janne Saarela <janne.juhani@gmail.com>
2019-02-09 12:19:43 +0200

    Start to add rebass v2 components as custom components, rebass v3 has removed many of their components

---
+++ components/RebassComponents/Divider.styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export default styled.div`
+  border: 0;
+  border-bottom: 1px solid;
+  border-color: #0077cc;
+  margin-left: 0px;
+  margin-right: 0px;
+  margin-top: 16px;
+  margin-bottom: 16px;
+`;
