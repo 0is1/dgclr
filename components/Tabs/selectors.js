e804c9970bb95b25559ee2e6871296968826e026
Janne Saarela <janne.juhani@gmail.com>
2019-02-10 22:01:10 +0200

    Return null if not found

--- components/Tabs/selectors.js
+++ components/Tabs/selectors.js
@@ -3,9 +3,10 @@ import { get } from 'lodash/fp';
 import type { State } from 'lib/types';
 
 export const getActiveIndex = (state: State, ownProps: { id: string }) => {
+  if (!ownProps || !ownProps.id) return null;
   const tabData = get(['tabs', 'data', ownProps.id], state);
   if (tabData) return tabData.activeIndex;
-  return false;
+  return null;
 };
 
 export default getActiveIndex;
