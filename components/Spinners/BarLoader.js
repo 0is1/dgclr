f492d597ceaecca21d58a0dfc6a5e1914fb70acd
Janne Saarela <janne.juhani@gmail.com>
2019-08-04 13:02:11 +0300

    Fix imports

--- components/Spinners/BarLoader.js
+++ components/Spinners/BarLoader.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import { BarLoader } from 'react-spinners';
+import BarLoader from 'react-spinners/BarLoader';
 import { Box, Flex } from 'rebass';
 import colors from 'components/colors';
 
 const Spinner = () => (
   <Flex alignItems="center" justifyContent="center">
     <Box>
-      <BarLoader
-        className="loading"
-        sizeUnit="%"
-        width={80}
-        height={8}
-        color={colors.info}
-        loading
-      />
+      <BarLoader className="loading" sizeUnit="%" width={80} height={8} color={colors.info} loading />
     </Box>
   </Flex>
 );
