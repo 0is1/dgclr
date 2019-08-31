15a0fcd142c86e2fd353b9c74e3878f36711d6fa
Janne Saarela <janne.juhani@gmail.com>
2019-08-31 19:46:15 +0300

    Add logic to update url query dynamically

--- components/AdvancedSearch/AdvancedSearch.js
+++ components/AdvancedSearch/AdvancedSearch.js
@@ -1,14 +1,17 @@
 // @flow
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
+import Router from 'next/router';
+import Link from 'next/link';
 import { Box } from 'rebass';
 import type { State as ReduxState } from 'lib/types';
 import AdvancedSearchInputs from './AdvancedSearchInputs';
 import AdvancedSearchQuery from './AdvancedSearchQuery';
 import { getCurrentAdvancedFilter } from './selectors';
-import Styles from './AdvancedSearchContainer.styles';
+import Styles from './AdvancedSearch.styles';
 
-const { Wrapper } = Styles;
+const { TextSearchLink, Wrapper } = Styles;
+const FILTER_UPDATED = 'FILTER_UPDATED';
 
 type Props = {};
 type MapStateToProps = { filter: string };
@@ -17,17 +20,40 @@ type MapDispatchToProps = {};
 type CombinedProps = Props & MapStateToProps & MapDispatchToProps;
 
 class AdvancedSearch extends PureComponent<CombinedProps> {
-  toggleOpen = () => {
-    // TODO: link
-  };
+  componentDidMount() {
+    const { pathname, query } = Router;
+    const { filter } = this.props;
+    if (query && Object.keys(query).length === 0 && filter) {
+      const href = `${pathname}?q=${filter}`;
+      Router.push(href, href, { shallow: true });
+    }
+  }
 
-  handleKeyPress = () => {};
+  componentDidUpdate(prevProps: CombinedProps, prevState: any, snapshot: any) {
+    if (snapshot === FILTER_UPDATED) {
+      const { filter } = this.props;
+      const { pathname } = Router;
+      const href = `${pathname}?q=${filter}`;
+      Router.push(href, href, { shallow: true });
+    }
+  }
+
+  getSnapshotBeforeUpdate(prevProps: CombinedProps) {
+    const { filter } = this.props;
+    if (prevProps.filter !== filter) {
+      return FILTER_UPDATED;
+    }
+    return null;
+  }
 
   render() {
     const { filter } = this.props;
     return (
       <Wrapper>
-        <Box m="1rem auto" px="2rem" width={[1, 1, 1, 0.7]}>
+        <Box style={{ position: 'relative' }} m="1rem auto" px="2rem" width={[1, 1, 1, 0.7]}>
+          <Link href="/">
+            <TextSearchLink>Tekstihaku</TextSearchLink>
+          </Link>
           <AdvancedSearchInputs />
           <AdvancedSearchQuery filter={JSON.parse(filter)} />
         </Box>
