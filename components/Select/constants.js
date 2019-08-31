d66824a129850dea4c04f4e1265c37dbf83d709e
Janne Saarela <janne.juhani@gmail.com>
2019-08-31 19:28:58 +0300

    Add/edit/remove constants

---
+++ components/Select/constants.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line import/prefer-default-export
+export const SELECT_FILTER_NAMES = {
+  basketType: { filterName: 'basketType' },
+  courseTypes: { filterName: 'courseTypes' },
+  rating: { filterName: 'rating' },
+  surfaceShapeTypes: { filterName: 'surfaceShapeTypes' },
+  teeType: { filterName: 'teeType' },
+};
