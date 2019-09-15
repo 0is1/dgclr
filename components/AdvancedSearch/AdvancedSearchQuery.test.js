51078c1b8b09710c7d56fe55bcf33001c6b234e1
Janne Saarela <janne.juhani@gmail.com>
2019-09-15 20:12:25 +0300

    Add test with data

--- components/AdvancedSearch/AdvancedSearchQuery.test.js
+++ components/AdvancedSearch/AdvancedSearchQuery.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import mockCoursesData from 'components/SearchContainer/mock/courses.mock';
 import { AdvancedSearchQuery } from './AdvancedSearchQuery';
 
 const mockProps = (props = {}) => ({
@@ -16,6 +17,11 @@ describe('<AdvancedSearchQuery />', () => {
   describe('snapshots', () => {
     it('renders with default props', () => {
       const props = mockProps();
+      const subject = shallow(<AdvancedSearchQuery {...props} />);
+      expect(subject).toMatchSnapshot();
+    });
+    it('renders with queryResults', () => {
+      const props = mockProps({ queryResults: mockCoursesData });
       const subject = shallow(<AdvancedSearchQuery {...props} />);
       expect(subject).toMatchSnapshot();
     });
