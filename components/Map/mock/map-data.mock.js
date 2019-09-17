b25c4d9470a1bf34df7b287802faf70fcd19170c
Janne Saarela <janne.juhani@gmail.com>
2019-09-17 09:58:29 +0300

    Add mock data

---
+++ components/Map/mock/map-data.mock.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line import/prefer-default-export
+export const filterQueryResultsForMapMock = [
+  {
+    address: 'Kilpailukuja, 04300 Tuusula',
+    coordinates: { lat: 60.3905, lng: 25.03 },
+
+    id: '5cfd0e367a3430892fcde89d',
+    name: 'Ford DiscGolfPark',
+    slug: 'ford-discgolfpark-tuusula',
+  },
+  {
+    address: 'Keinukalliontie 47, 04250 Kerava',
+    coordinates: {
+      lat: 60.3846,
+      lng: 25.1396,
+    },
+    id: '5cfd0e7f7a3430892fcde9c6',
+    name: 'Keinukallio',
+    slug: 'keinukallio-kerava',
+  },
+];
