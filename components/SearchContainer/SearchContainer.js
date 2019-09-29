75cbd96f2cf349a52653243ca0b1e77825ca7150
Janne Saarela <janne.juhani@gmail.com>
2019-09-29 15:15:38 +0300

    USe translations

--- components/SearchContainer/SearchContainer.js
+++ components/SearchContainer/SearchContainer.js
@@ -5,6 +5,7 @@ import { debounce } from 'lodash';
 import { Box } from 'rebass';
 import Input from 'components/Input';
 import RebassComponents from 'components/RebassComponents';
+import { withTranslation } from 'i18n';
 import type { State as ReduxState } from 'lib/types';
 import SearchQuery from './SearchQuery';
 import { latestQuery as latestQueryFunc } from './selectors';
@@ -13,7 +14,7 @@ import Styles from './SearchContainer.styles';
 const { Lead } = RebassComponents;
 const { Wrapper } = Styles;
 
-type Props = {};
+type Props = { t: Function };
 type MapStateToProps = { latestQuery: string };
 
 type MapDispatchToProps = {};
@@ -48,12 +49,13 @@ class SearchContainer extends PureComponent<CombinedProps, State> {
   };
 
   render() {
+    const { t } = this.props;
     const { query, inputValue } = this.state;
     return (
       <Wrapper>
         <Box style={{ position: 'relative' }} m="1rem auto" px="2rem" width={[1, 1, 1, 0.7]}>
-          <Lead my={2}>Etsi frisbeegolfratoja:</Lead>
-          <Input focusOnMount placeholder="Kaupungin tai radan nimi" value={inputValue} onChange={this.onSearchQueryChange} />
+          <Lead my={2}>{t('search:title')}</Lead>
+          <Input focusOnMount placeholder={t('search:input-placeholder')} value={inputValue} onChange={this.onSearchQueryChange} />
           <SearchQuery query={query} />
         </Box>
       </Wrapper>
@@ -65,4 +67,4 @@ const mapStateToProps = (state: ReduxState): MapStateToProps => ({
   latestQuery: latestQueryFunc(state),
 });
 
-export default connect<CombinedProps, Props, any, any, any, Function>(mapStateToProps)(SearchContainer);
+export default connect<CombinedProps, Props, any, any, any, Function>(mapStateToProps)(withTranslation('search')(SearchContainer));
