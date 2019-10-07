7e9397cb656c13b40e96087162f416bd8c4c5d0f
Janne Saarela <janne.juhani@gmail.com>
2019-10-07 14:42:21 +0300

    Add tests for convertWWWToHttpAndAddLinks

--- helpers/utils.test.js
+++ helpers/utils.test.js
@@ -11,6 +11,7 @@ import {
   getTitle,
   isArrayWithLength,
   uniqueLayoutRatings,
+  convertWWWToHttpAndAddLinks,
 } from './utils';
 
 describe('Utils', () => {
@@ -25,6 +26,15 @@ describe('Utils', () => {
       expect(result).toBeDefined();
       expect(result).toEqual(expect.not.stringMatching('<a href='));
       expect(result).toEqual(mockCourses[1].description);
+    });
+    // TODO: do not include dot at the end of url
+    it.skip('convert http-text to link without last dot', () => {
+      const string = 'Rata on perustettu vuonna 1998 vanhan golfkentän paikalle, ja se laajennettiin täysimittaiseksi 18-väyläiseksi vuonna 2004. Hiirosessa on pelattu useita SM-tason kilpailuja. Rata soveltuu sekä kilpapelaajille että harrastajille: osa väylistä on haastavia metsäväyliä, mutta tarjolla on myös lyhyempiä puistoväyliä. Radan pituus on 1999 metriä ja par 58. Väylien pituudet vaihtelevat 69:stä 243metriin. Hiirosen frisbeegolfrataa hoitavat oululaiset seurat Oulun Frisbeeseura ry ja BSC Disc Golf Team ry yhteistyössä. Oulun kaupunki huolehtii ruohon ja heinän leikkuusta säännöllisesti. Radan yhteydessä on riittävästi paikoitustilaa, jonka sijainti käy ilmi ratakartasta. Alue on yleinen puisto, joten muiden liikkujien ja toisten turvallisuus on huomioitava erityisen tarkasti. WC-tiloja radan yhteydessä ei ole. Lähimmät palvelut ovat Hiirosen Neste ja Maikkulan Neste. Radalla pelataan viikkokisoja, joissa ennen kisan alkua on paikalla myös välinemyynti. Katso tarkemmat tiedot osoitteesta www.viikkokisat.com. Ratakartta, väyläesittelyt ja ajo-ohjeet http://www.oulunfrisbeeseura.net/radat.php?rata=hiironen. Tervetuloa Hiiroseen!';
+      const result = convertLinksToHtml(string);
+      console.log('result: ', result);
+      expect(result).toEqual(
+        "Rata on perustettu vuonna 1998 vanhan golfkentän paikalle, ja se laajennettiin täysimittaiseksi 18-väyläiseksi vuonna 2004. Hiirosessa on pelattu useita SM-tason kilpailuja. Rata soveltuu sekä kilpapelaajille että harrastajille: osa väylistä on haastavia metsäväyliä, mutta tarjolla on myös lyhyempiä puistoväyliä. Radan pituus on 1999 metriä ja par 58. Väylien pituudet vaihtelevat 69:stä 243metriin. Hiirosen frisbeegolfrataa hoitavat oululaiset seurat Oulun Frisbeeseura ry ja BSC Disc Golf Team ry yhteistyössä. Oulun kaupunki huolehtii ruohon ja heinän leikkuusta säännöllisesti. Radan yhteydessä on riittävästi paikoitustilaa, jonka sijainti käy ilmi ratakartasta. Alue on yleinen puisto, joten muiden liikkujien ja toisten turvallisuus on huomioitava erityisen tarkasti. WC-tiloja radan yhteydessä ei ole. Lähimmät palvelut ovat Hiirosen Neste ja Maikkulan Neste. Radalla pelataan viikkokisoja, joissa ennen kisan alkua on paikalla myös välinemyynti. Katso tarkemmat tiedot osoitteesta www.viikkokisat.com. Ratakartta, väyläesittelyt ja ajo-ohjeet <a href='http://www.oulunfrisbeeseura.net/radat.php?rata=hiironen'>http://www.oulunfrisbeeseura.net/radat.php?rata=hiironen</a>. Tervetuloa Hiiroseen!",
+      );
     });
   });
   describe('convertCoordinatesToObject', () => {
@@ -154,6 +164,30 @@ describe('Utils', () => {
       expect(convertKilometersToMeters(null)).toEqual(0);
       expect(convertKilometersToMeters(0)).toEqual(0);
       expect(convertKilometersToMeters(undefined)).toEqual(0);
+    });
+  });
+  describe('convertWWWToHttpAndAddLinks', () => {
+    it('return original string if no www.domain.com texts found', () => {
+      const string = 'Tässä tekstissä ei ole yhtään www-alkuista "linkkiä". Sen sijaan tässä on yksi <a href="https://www.dgclr.fi"><a>-elementti</a>. Sen ei siis pitäisi muuttua convertWWWToHttpAndAddLinks-funktiossa.';
+      expect(convertWWWToHttpAndAddLinks(string)).toEqual(string);
+    });
+    it('convert string with www.domain.com text to links', () => {
+      const string = "Tämä ei pitäisi muuntua https://www.dgclr.fi. Sen sijaan tämä www.frisbeegolfrata.info pitäisi muuttua linkiksi. Meri-Toppilan frisbeegolfrata sijaitsee Oulussa n. viiden kilometrin päässä keskustasta Meri-Toppilan kaupunginosassa meren rannassa. Rata perustettiin vuonna 2007 ja se on saavuttanut suuren suosion jo lyhyen olemassaolonsa aikana. Radalla on pelattu useita valtakunnallisia kilpailuja ja kisa toimi vuoden 2016 Euroopanmestaruuskisojen näyttämönä. Radalla on kesällä 2017 alkaen käynnissä muutostöitä, kun läheisen asuinalueen tieltä kaivettavia maita läjitetään puistoon. Muutostöiden aikana on käytössä tilapäinen ratalayout: Radan yhteydessä ei ole WC-tiloja. Lähimmät palvelut ovat Meri-Toppilan asutuskeskuksessa sijaitsevat  kioski ja baari-pizzeria. Pysäköintipaikat ovat aivan radan vieressä, lähimmät ykköstiin vieressä. Ykköstiin vierestä löytyy myös löytökiekkolaatikko. Meri-Toppilan ulkoilualue on yleinen puisto. Tästä syystä muut puiston käyttäjät on huomioitava erityisen tarkasti. Radalla pelataan viikkokisoja. Katso tarkemmat tiedot osoitteesta www.viikkokisat.com. Hyvä tietää: Rata sijaitsee niittyluokitellulla alueella, ja väylät niitetään ensimmäisen kerran kesäkuun lopulla ja koko alueen heinät niitetään elokuun puolivälissä. Tästä syystä heinä on paikoin pitkää ja pelaamista haittaavaa kesäkuun puolivälistä elokuun puoliväliin. Meri-Toppilan 18-väyläistä hoitavat oululaiset seurat Oulun Frisbeeseura ry ja BSC Disc Golf Team ry yhteistyössä. Tämän ei pitäisi vaikuttaa lopputulokseen <a href='https://www.google.com'>https://www.google.com</a>.";
+      const result = convertWWWToHttpAndAddLinks(string);
+      expect(result).toEqual(
+        "Tämä ei pitäisi muuntua https://www.dgclr.fi. Sen sijaan tämä <a href='http://www.frisbeegolfrata.info'>www.frisbeegolfrata.info</a> pitäisi muuttua linkiksi. Meri-Toppilan frisbeegolfrata sijaitsee Oulussa n. viiden kilometrin päässä keskustasta Meri-Toppilan kaupunginosassa meren rannassa. Rata perustettiin vuonna 2007 ja se on saavuttanut suuren suosion jo lyhyen olemassaolonsa aikana. Radalla on pelattu useita valtakunnallisia kilpailuja ja kisa toimi vuoden 2016 Euroopanmestaruuskisojen näyttämönä. Radalla on kesällä 2017 alkaen käynnissä muutostöitä, kun läheisen asuinalueen tieltä kaivettavia maita läjitetään puistoon. Muutostöiden aikana on käytössä tilapäinen ratalayout: Radan yhteydessä ei ole WC-tiloja. Lähimmät palvelut ovat Meri-Toppilan asutuskeskuksessa sijaitsevat  kioski ja baari-pizzeria. Pysäköintipaikat ovat aivan radan vieressä, lähimmät ykköstiin vieressä. Ykköstiin vierestä löytyy myös löytökiekkolaatikko. Meri-Toppilan ulkoilualue on yleinen puisto. Tästä syystä muut puiston käyttäjät on huomioitava erityisen tarkasti. Radalla pelataan viikkokisoja. Katso tarkemmat tiedot osoitteesta <a href='http://www.viikkokisat.com'>www.viikkokisat.com</a>. Hyvä tietää: Rata sijaitsee niittyluokitellulla alueella, ja väylät niitetään ensimmäisen kerran kesäkuun lopulla ja koko alueen heinät niitetään elokuun puolivälissä. Tästä syystä heinä on paikoin pitkää ja pelaamista haittaavaa kesäkuun puolivälistä elokuun puoliväliin. Meri-Toppilan 18-väyläistä hoitavat oululaiset seurat Oulun Frisbeeseura ry ja BSC Disc Golf Team ry yhteistyössä. Tämän ei pitäisi vaikuttaa lopputulokseen <a href='https://www.google.com'>https://www.google.com</a>.",
+      );
+    });
+    it('convert string with www.domain.com/path text to links', () => {
+      const string = 'Radan sivut Facebookissa: www.facebook.com/tarinalaakso. Ajo-ohje: Nouse Siilinjärven keskustan rampista ja lähde suuntaan Viitasaari/Maaninka.  Noin kilometrin päästä käänny vasemmalle suuntaan Kumpunen. Kentän parkkialue sijaitsee heti risteyksen jälkeen oikealla puolella. Ykköstii ja opastaulu hiekkamontussa. Kentällä ei ole WC-tiloja. Lähin WC löytyy Siilinjärven keskustasta. Tervetuloa rohkeasti Viikkokisoihin!';
+      const result = convertWWWToHttpAndAddLinks(string);
+      expect(result).toEqual(
+        "Radan sivut Facebookissa: <a href='http://www.facebook.com/tarinalaakso'>www.facebook.com/tarinalaakso</a>. Ajo-ohje: Nouse Siilinjärven keskustan rampista ja lähde suuntaan Viitasaari/Maaninka.  Noin kilometrin päästä käänny vasemmalle suuntaan Kumpunen. Kentän parkkialue sijaitsee heti risteyksen jälkeen oikealla puolella. Ykköstii ja opastaulu hiekkamontussa. Kentällä ei ole WC-tiloja. Lähin WC löytyy Siilinjärven keskustasta. Tervetuloa rohkeasti Viikkokisoihin!",
+      );
+    });
+    it('convert string with www.domain.com/path?a=1&b=2 text to links', () => {
+      const result = convertWWWToHttpAndAddLinks('www.hello.world/subpath/another/?a=1&b=2');
+      expect(result).toEqual("<a href='http://www.hello.world/subpath/another/?a=1&b=2'>www.hello.world/subpath/another/?a=1&b=2</a>");
     });
   });
 });
