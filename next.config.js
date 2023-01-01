bb282921b3fcf67199b8de5a72354dc55758e24d
Janne Saarela <janne.juhani@gmail.com>
2023-01-01 17:18:57 +0200

    Add s3 images

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
-const { i18n } = require("./next-i18next.config");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { i18n } = require('./next-i18next.config');
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["frisbeegolfradat.fi", "malluunen-dg.vercel.app"],
+    domains: [
+      'frisbeegolfradat.fi',
+      'malluunen-dg.vercel.app',
+      'dgclr-server-course-maps-dev.s3.eu-central-1.amazonaws.com',
+    ],
   },
   i18n,
 };
