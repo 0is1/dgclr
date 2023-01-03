5439d48f708a405682489ee840cdfdbda8bdb5ae
Janne Saarela <janne.juhani@gmail.com>
2023-01-03 11:42:29 +0200

    Maybe fix translations

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
+const localePath = path.resolve('./public/locales');
+
 module.exports = {
   i18n: {
     defaultLocale: 'fi',
     locales: ['fi', 'en'],
   },
+  localePath,
+  reloadOnPrerender: process.env.NODE_ENV === 'development',
 };
